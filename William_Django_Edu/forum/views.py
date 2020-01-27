cf86177a0ced76d1d48bba8b0350ae63d189a320
williamjiamin <lxodgf@163.com>
2020-01-28 00:13:17 +0800

    write some jinjia2 for title

--- William_Django_Edu/forum/views.py
+++ William_Django_Edu/forum/views.py
@@ -13,15 +13,19 @@ posts = [
         'title': '这个是Mary发的帖子标题',
         'content': '这是这个帖子的内容',
         'date_posted': '2022,2,2'
-    },
+    }
 ]
 
 
 # def home(request):
 #     return HttpResponse('<h1>大家好，这个页面是乐学偶得论坛的主页</h1>')
 def home(request):
-    return render(request, 'forum/home.html')
+    context = {
+        'posts': posts,
+        'title': '主页'
+    }
+    return render(request, 'forum/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>大家好，这个页面是乐学偶得论坛的关于页面</h1>')
+    return render(request, 'forum/about.html', {'title': '关于页面'})
